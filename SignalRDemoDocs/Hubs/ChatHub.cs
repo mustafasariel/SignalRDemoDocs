9a274dc146e8387cedb608f691a4c42fe2df7565
mustafa sarıel <mustafasariel@hotmail.com>
2020-01-21 01:20:50 +0300

    Add project files.

---
+++ SignalRDemoDocs/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRDemoDocs.Hubs
+{
+    public class ChatHub:Hub
+    {
+        // Hub sınıfı bağlantıları, grupları ve mesajlaşmayı yönetir.
+        public async Task SendMessage(string user,string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+       
+    }
+}
